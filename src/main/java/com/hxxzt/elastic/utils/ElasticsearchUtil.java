4ab6367041cd1d68b764f3e9b3704a43b9dc3f3a
NikoHXX <niko_hxx@163.com>
2019-04-25 14:36:16 +0800

    SpringBoot整合ElasticSearch

--- src/main/java/com/hxxzt/elastic/utils/ElasticsearchUtil.java
+++ src/main/java/com/hxxzt/elastic/utils/ElasticsearchUtil.java
@@ -24,7 +24,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
